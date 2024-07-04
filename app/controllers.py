8013e122243b712f0e7cf208f94da47f82469115
vitoriatumelero <vitoriatumelero1807@gmail.com>
2024-07-04 14:19:27 -0300

    att

---
+++ app/controllers.py
@@ -0,0 +1,41 @@
+
+from app.models.aluno import Aluno
+from app.models.professor import Professor
+from app.models.disciplina import Disciplina
+from app.models.notas import Nota
+
+alunos = [
+    Aluno("Pedro Pedro", 23, "https://github.com/vitorrumke", "https://linkedin.com/in/vitorrumke", "Estudante de Engenharia.", "path/to/photo.jpg"),
+    Aluno("Alfredo Palavro", 20, "https://github.com/vitorrumke", "https://linkedin.com/in/vitorrumke", "Estudante de Engenharia.", "path/to/photo.jpg"),
+    Aluno("Ana Palavro", 18, "https://github.com/vitorrumke", "https://linkedin.com/in/vitorrumke", "Estudante de Engenharia.", "path/to/photo.jpg"),
+    Aluno("Alfredo Jaconi", 19, "https://github.com/vitorrumke", "https://linkedin.com/in/vitorrumke", "Estudante de Engenharia.", "path/to/photo.jpg"),
+    Aluno("Francisco Stedille", 22, "https://github.com/vitorrumke", "https://linkedin.com/in/vitorrumke", "Estudante de Engenharia.", "path/to/photo.jpg"),
+]
+
+professores = [
+    Professor("Edertec", "https://github.com/anacarolina", "https://linkedin.com/in/anacarolina", "Estudante de Engenharia.", "path/to/photo.jpg"),
+    Professor("Rafa", "https://github.com/anacarolina", "https://linkedin.com/in/anacarolina", "Estudante de Engenharia.", "path/to/photo.jpg"),
+    Professor("Cris", "https://github.com/anacarolina", "https://linkedin.com/in/anacarolina", "Estudante de Engenharia.", "path/to/photo.jpg"),
+]
+ 
+disciplinas = [
+    Disciplina("Matemática", professores[0]),
+    Disciplina("Física", professores[1]),
+    Disciplina("Progamação", professores[0]),
+    Disciplina("Games", professores[1]),
+    Disciplina("Banco de Dados", professores[2]),
+    # Adicione mais disciplinas conforme necessário
+]
+
+notas = [
+    Nota(alunos[0], disciplinas[0], 9.5),
+    Nota(alunos[0], disciplinas[1], 8.0),
+    Nota(alunos[0], disciplinas[2], 7.5),
+    Nota(alunos[0], disciplinas[3], 6.0),
+    Nota(alunos[0], disciplinas[4], 8.5),
+    Nota(alunos[1], disciplinas[1], 7.0),
+    Nota(alunos[1], disciplinas[2], 7.5),
+    Nota(alunos[1], disciplinas[3], 6.0),
+    Nota(alunos[1], disciplinas[4], 8.5),
+    # Adicione mais notas conforme necessário
+]
---
+++ app/models/professor.py
@@ -0,0 +1,8 @@
+class Professor:
+    def __init__(self, nome, github, linkedin, bio, foto):
+        self.nome = nome
+        self.github = github
+        self.linkedin = linkedin
+        self.bio = bio
+        self.foto = foto
+
